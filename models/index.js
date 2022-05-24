c13add7cb1b1b5416379ced6a2d3e1d73608e7eb
Michael Kendrick <michael@locumsmart.net>
2022-05-24 12:37:32 -0500

    Starting

---
+++ models/index.js
@@ -0,0 +1,29 @@
+// import models
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "",
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "",
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: "",
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: "",
+});
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
